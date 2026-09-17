eb8d83a435f2d79c5130f7b3e547f4db978f52f9
Elkhafaji <khafaji2026@gmail.com>
2026-09-17 15:01:37 +0300

    Delete components directory

--- components/StoreShell.tsx
+++
@@ -1,2 +0,0 @@
-import Header from './Header'; import Footer from './Footer';
-export default function StoreShell({children}:{children:React.ReactNode}){return <><Header/><main>{children}</main><Footer/></>}
